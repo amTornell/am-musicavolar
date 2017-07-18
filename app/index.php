2be19696b1393d0bf9ac1b1245704a22fdae7583
amTornell <desireetornell@RMEX07BECM46.local>
2017-07-18 18:27:52 -0500

    php con menu

---
+++ app/index.php
@@ -0,0 +1,335 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8"><!-- utf-8 works for most cases -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta content="IE=edge" http-equiv="X-UA-Compatible">
+    <title>La música te hace volar</title>
+    <meta property="og:title" content="La música te hace volar"/>
+    <meta property="og:url" content="http://vuela.aeromexico.com/musica-te-hace-volar/"/>
+    <meta property="og:description" content="Aeroméxico"/>
+    <meta property="og:image" content="http://vuela.aeromexico.com/musica-te-hace-volar/images/facebook-share.jpg"/>
+    <meta property="og:image:type" content="image/jpeg" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+    <link rel="shortcut icon" type="image/x-icon" href="http://vuela.aeromexico.com/favicon.ico">
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <!-- Google Tag Manager -->
+    <noscript>
+        <iframe src="//www.googletagmanager.com/ns.html?id=GTM-PTTVTM" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                '//www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-PTTVTM');
+    </script>
+    <!-- header-->
+    <?php require_once('../E4menu/menutop.php'); ?> 
+    <section class="wrapper-logo">
+        <span class="the-logo animated fadeInUp">
+          <p class="text-centered">
+            <a href="">
+             <img src="images/musica_logo.png" alt="La música te hace volar" />
+            </a>
+          </p>
+        </span> 
+      </section>
+    <!-- / header-->
+    <!-- menu desktop -->
+      <nav id="nav-main" class="menu">
+        <ul>
+          <li class="home"><a rel="item">Home</a> &bull; </li>
+          <li class="playlists"><a rel="item-playlist">Playlists</a> &bull; </li>
+          <li class="videos"><a rel="item-youtube">Videos</a> &bull; </li>
+          <li class="instagram"><a rel="item-instagram">Instagram</a> &bull; </li>
+          <li class="articulos"><a rel="item-articulos">Artículos</a></li>
+          <!--<li class="festivales"><a rel="item-festivales">Festivales</a></li>-->
+        </ul>
+      </nav>
+      <!-- /menu desktop -->
+    <!-- dynamic -->
+    <div class="container-center animated fadeInUp">
+      <div class="row">
+      <!--item-->
+        <div class="col-md-12 item item-youtube">
+         <article>
+            <div class="col-md-8 no-padding">
+              <div class="videoWrapper">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/blj2pu20ynU?modestbranding=1&autohide=1&showinfo=0" frameborder="0" allowfullscreen></iframe>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <h2>LOS MEJORES FESTIVALES DEL NORTE | WiSH Outdoor, Beyond Wonderland | Festival Passport</h2>
+               <summary>
+                 <p>Descubre porque Monterrey esta rompiéndola con grandes festivales.
+                    <strong>#LaMusicaMeHaceVolar</strong><br>
+                    ¡Gracias Aeromexico por hacerlo posible!
+                </p>
+               </summary>
+            </div>  
+         </article>
+        </div>
+        <!-- /item-->
+        <!-- item-->
+        <div class="col-md-4 item item-instagram">
+         <article >
+          <iframe src="//instagram.com/p/BU2EFVpFw5M/embed/" width="612" height="376" frameborder="0" scrolling="no" allowtransparency="true"></iframe>
+         </article>
+        </div>
+        <!-- / item -->
+        <!-- /item-->
+        <div class="col-md-4 item item-instagram">
+         <article >
+          <iframe src="//instagram.com/p/BUV_qPohla3/embed/" width="612" height="612" frameborder="0" scrolling="no" allowtransparency="true"></iframe>
+         </article>
+        </div>
+        <!-- / item -->
+          <!-- item-->
+        <div class="col-md-4 item item-instagram">
+         <article >
+         <iframe src="//instagram.com/p/BUXM9rOFUL0/embed/" width="612" height="612" frameborder="0" scrolling="no" allowtransparency="true"></iframe>
+         </article>
+        </div>
+        <!-- / item -->
+        <!--item-->
+        <div class="col-md-8 item item-articulos">
+         <article style="background-image: url(images/bravo_nota.jpg);">
+            <header>
+              <span class="icon">
+                <img src="images/musica_notas.png" alt="icono" />
+              </span>
+              <p class="date">
+                <small>Junio 2017</small>
+              </p>
+            </header>
+            <div class="item-information">
+              <h2>¡Festival Valle de Bravo!</h2>
+               <summary>
+                 <p>Crónica de un festival de música en medio del bosque.</p>
+               </summary>
+               <footer>
+                 <a href="https://www.aeromexico.com/es-mx/blog-de-viajes-inspirate/festival-valle-de-bravo" class="btn btn-main">
+                   Conoce más
+                 </a>
+               </footer>
+            </div>
+         </article>
+        </div>
+        <!--item-->
+                  <!-- spotify-->
+        <div class="col-md-4 item item-playlist">
+         <article style="background-image: url(images/bravo_spotify.jpg);">
+            <header>
+              <span class="icon">
+                <img src="images/musica_spotify.png" alt="icono" />
+              </span>
+              <p class="date">
+                <small>Junio 2017</small>
+              </p>
+            </header>
+            <div class="item-information">
+              <h2>Escucha nuestra selección de Bravo 2017</h2>
+               <summary>
+                 <p></p>
+               </summary>
+               <footer>
+                 <a href="https://open.spotify.com/user/soyaeromexico/playlist/4tSiNbBigHLrfGkJynjz3e" class="btn btn-main">
+                   Escúchala
+                 </a>
+               </footer>
+            </div>
+         </article>
+        </div>
+        <!-- / spotify-->
+        <div class="col-md-12 item item-youtube">
+         <article>
+            <div class="col-md-8 no-padding">
+              <div class="videoWrapper">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/KxYXXgjB1oc?modestbranding=1&autohide=1&showinfo=0" frameborder="0" allowfullscreen></iframe>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <h2>BRAVO FESTIVAL ¡ME TIRÉ DE UN PARAPENTE! (Ft. Fernando Lozada, Muriel Ebright) | Festival Passport</h2>
+               <summary>
+                 <p>Esta es una de las experiencias más extremas, divertidas y enigmáticas de mi vida. ¡Me tiré del parapente! ¡Y Bravo festival fue simplemente mágico! Un evento que te explota todos tus sentidos.<br>
+                  Me encanto convivir con la naturaleza y la música.</p>
+               </summary>
+            </div>  
+         </article>
+        </div>
+        <!-- /item-->
+
+        
+        <!--item-->
+        <div class="col-md-8 item item-articulos">
+         <article style="background-image: url(images/musica_video_ceremonia.jpg);">
+            <header>
+              <span class="icon">
+                <img src="images/musica_notas.png" alt="icono" />
+              </span>
+              <p class="date">
+                <small>Junio 2017</small>
+              </p>
+            </header>
+            <div class="item-information">
+              <h2>Te contamos cómo estuvo el Ultra Music Festival</h2>
+               <summary>
+                 <p>No creo que exista un sentimiento más hermoso que despegar rumbo a una gran aventura. Hoy es un sueño que comparto en cada video con miles de personas.</p>
+               </summary>
+               <footer>
+                 <a href="https://www.aeromexico.com/es-mx/blog-de-viajes-inspirate/te-contamos-como-estuvo-el-ultra-music-festival?utm_source=landingmusica&utm_medium=landing&utm_content=branding&utm_campaign=lamusicatehacevolar&utm_term=mx-proymusica" class="btn btn-main">
+                   Conoce más
+                 </a>
+               </footer>
+            </div>
+         </article>
+        </div>
+        <!-- /item-->
+        <div class="col-md-4 item item-articulos">
+         <article style="background-image: url(images/musica_post_ultramusic2.jpg);">
+          <header>
+            <span class="icon">
+              <img src="images/musica_notas.png" alt="icono" />
+            </span>
+            <p class="date">
+              <small>Abril 2017</small>
+            </p>
+          </header>
+          <div class="item-information">
+             <h2>10 consejos para disfrutar el Ultra Music Festival de Miami </h2>
+             <summary>
+               <p>Aquí te decimos todo lo que debes saber si vas al festival mas top del mundo.</p>
+             </summary>
+             <footer>
+               <a href="https://www.aeromexico.com/es-mx/blog-de-viajes-inspirate/10-consejos-para-disfrutar-el-ultra-music-festival-de-miami?utm_source=landingmusica&utm_medium=landing&utm_content=branding&utm_campaign=lamusicatehacevolar&utm_term=mx-blogultramiami" class="btn btn-main">
+                 Conoce más
+               </a>
+             </footer>
+          </div>   
+         </article>
+        </div>
+         <!-- /item-->
+         <div class="col-md-12 item item-youtube">
+             <article>
+                <div class="col-md-8 no-padding">
+                  <div class="videoWrapper">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/0-ZqF6Psl_0?modestbranding=1&autohide=1&showinfo=0" frameborder="0" allowfullscreen></iframe>
+                  </div>
+                </div>
+                <div class="col-md-4">
+                  <h2>ULTRA MUSIC FESTIVAL 2017 🌈🎵🌴 TODO EL FIN EN MIAMI 🔥😈 | Festival Passport</h2>
+                   <summary>
+                     <p>No se pierdan este video donde les muestro como se vivió Ultra, y los llevo conmigo de AFTER a los mejores clubs de Miami durante Miami Music Week.<p>
+                   </summary>
+                </div>  
+             </article>
+        </div>
+        <!-- /item-->
+        <!-- /item-->
+         <div class="col-md-12 item item-youtube">
+             <article>
+                <div class="col-md-8 no-padding">
+                  <div class="videoWrapper">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/lVSnSYVbgIY?modestbranding=1&autohide=1&showinfo=0" frameborder="0" allowfullscreen></iframe>
+                  </div>
+                </div>
+                <div class="col-md-4">
+                  <h2>Ceremonia 2017 | #LaMúsicaMeHaceVolar</h2>
+                   <summary>
+                     <p>Únete a nuestro movimiento #LaMúsicaMeHaceVolar<br>
+
+                      Así fue como vivimos Ceremonia y, aunque este increíble evento llegó a su fin, nos quedan las memorias. Revivámoslo juntos una y otra vez y recordemos que la música nos hace volar.<br><br>
+
+                      ¡No olvides seguirnos en nuestras redes sociales!<br>
+                      Snapchat: SoyAeromexico<br>
+                     <a href="https://www.facebook.com/AeromexicoMX" class="link"> Facebook </a><br>
+                       <a href="https://twitter.com/Aeromexico" class="link">Twitter</a><br>
+                       <a href="https://www.instagram.com/aeromexico/" class="link">Instagram</a>
+                      </p>
+                   </summary>
+                </div>  
+             </article>
+        </div>
+        <!-- /item-->
+        <!-- /item-->
+        <div class="col-md-6 item item-spotify">
+         <article style="background-image: url(images/musica_post_ultramusic2.jpg);">
+          <header>
+            <span class="icon">
+                <img src="images/musica_spotify.png" alt="icono" />
+              </span>
+            <p class="date">
+              <small>Abril 2017</small>
+            </p>
+          </header>
+          <div class="item-information">
+             <h2>Escucha nuestra selección de Ceremonia 2017 </h2>
+           
+             <footer>
+               <a href="https://open.spotify.com/user/soyaeromexico/playlist/0wd8vaqYmjkx9J0CKvBQ51" class="btn btn-main">
+                 Escúchala
+               </a>
+             </footer>
+          </div>   
+         </article>
+        </div>
+        <!-- /item-->
+         <!-- /item-->
+        <div class="col-md-6 item item-spotify">
+         <article style="background-image: url(images/musica_post_ultramusic2.jpg);">
+          <header>
+            <span class="icon">
+                <img src="images/musica_spotify.png" alt="icono" />
+              </span>
+            <p class="date">
+              <small>Abril 2017</small>
+            </p>
+          </header>
+          <div class="item-information">
+             <h2>Escucha nuestra selección de Hello Fest 2017</h2>
+           
+             <footer>
+               <a href="https://open.spotify.com/user/soyaeromexico/playlist/5IC97xJEfsAaLb5xtqNsZd" class="btn btn-main">
+                 Escúchala
+               </a>
+             </footer>
+          </div>   
+         </article>
+        </div>
+        <!-- /item-->
+      </div>
+    </div>
+    <!-- footer -->
+      <!-- header-->
+  <?php require_once('../E4menu/footer.php'); ?> 
+    <!-- / header-->
+
+  <!-- scripts -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+  <!--<script async defer src="//platform.instagram.com/en_US/embeds.js"></script>-->
+  <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+  <script src="js/vendor/bootstrap.min.js"></script>
+  
+  <script src="js/app.js"></script>
+
+</body>
+</html>
+
+
+
